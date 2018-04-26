c333f59af729bc6364d2fd091bb6badc809689b3
Satya Ajay Ravuri <30391209+satyaajayr@users.noreply.github.com>
2018-04-27 01:26:16 +0530

    ps2

---
+++ PS2/SparkProblemStatement2/src/com/upgrad/Spark/ProblemStatement2.java
@@ -0,0 +1,41 @@
+package com.upgrad.Spark;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaPairRDD;
+//import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.JavaSparkContext;
+//import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+//import scala.Tuple1;
+//import scala.Tuple10;
+//import scala.Tuple2;
+
+
+public class ProblemStatement2 {
+
+	public static void main(String[] args) {
+		System.setProperty("hadoop.home.dir", "C:\\winutil");
+
+		Logger.getLogger("org").setLevel(Level.ERROR);
+		SparkSession session = SparkSession.builder().appName("ProblemStatement2").master("local[*]").getOrCreate();		
+		DataFrameReader dataframeReader = session.read();
+		
+		Dataset<Row> ratings = dataframeReader.option("header","true").csv("in/trip_yellow_taxi.data");		
+		Dataset<Row> ratingsWithoutHeader = ratings.filter("VendorID == 1 or VendorID == 2");		
+		Dataset<Row> filteredRatings = ratingsWithoutHeader.filter("RatecodeID==4");
+		
+		filteredRatings.rdd().saveAsTextFile("ProblemStatement2");
+		
+	}
+
+}
